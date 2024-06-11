489f79ed875ecbd36b8c1d9ddbd7b6a5a94a06f0
Bear <programming@hallhome.us>
2024-06-11 07:21:22 -0500

    Add files via upload
    initial seed

---
+++ helper.php
@@ -0,0 +1,10 @@
+<?php
+
+// no direct access
+defined('_JEXEC') or die('Restricted access');
+
+require_once (JPATH_SITE.DS.'components'.DS.'com_content'.DS.'src'.DS.
+  'Service'.DS.'Router.php');
+
+
+?>
