737d14393082496944177041dcd88c393313c44f
Olivier Buisard <olivier.buisard@simplifyyourweb.com>
2024-06-11 10:29:39 -0400

    Update default.php
    Use the web asset manager
    Added parameter to get the license key

--- tmpl/default.php
+++ tmpl/default.php
@@ -11,8 +11,17 @@
 // no direct access
 defined('_JEXEC') or die('Restricted access');
 
+use Joomla\CMS\Factory;
 use Joomla\CMS\HTML\HTMLHelper;
 
-?>
+$wam = Factory::getApplication()->getDocument()->getWebAssetManager();
 
-<script id="ze-snippet" src="https://static.zdassets.com/ekr/snippet.js?key=a8506cba-c95f-4e73-8f8e-ec3f2474f3d3"> </script>
+$scriptUrl  = 'https://static.zdassets.com/ekr/snippet.js';
+$licenseKey = $params->get('license_key', '');
+
+if ($licenseKey) {
+    $scriptUrl .= '?key=' . $licenseKey;
+}
+
+$wam->registerAndUseScript('ze-snippet', $scriptUrl, [], ['defer' => true]);
+?>
