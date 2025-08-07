2e15bc00c064c91e9e9c023beae5e85930ba6c89
Bear <programming@hallhome.us>
2025-08-07 11:40:53 -0500

    update to use "support' instead of help

--- tmpl/default.php
+++ tmpl/default.php
@@ -1,18 +1,39 @@
 <?php
 /**
- * @package    MOD_zendesk
+ * @package MOD_bearslivesearch
  *
- * @author     N6REJ programming@hallhome.us
- * @copyright  Copyright (c) 2024 N6REJ all rights reserved
- * @license    GNU General Public License version 3 or later; see LICENSE.txt
- * @link       http://n6rej.github.io
+ * @author N6REJ programming@hallhome.us
+ * @copyright Copyright (c) 2024 N6REJ all rights reserved
+ * @license GNU General Public License version 3 or later; see LICENSE.txt
+ * @link http://n6rej.github.io
  */
 
 // no direct access
 defined('_JEXEC') or die('Restricted access');
+
 use Joomla\CMS\HTML\HTMLHelper;
 
-$scriptUrl  = 'https://static.zdassets.com/ekr/snippet.js?key=';
+$scriptUrl = 'https://static.zdassets.com/ekr/snippet.js?key=';
 $scriptUrl .= $params->get('license_key', '');
 ?>
-<script id="ze-snippet" src="<?php echo $scriptUrl; ?>"> </script>
+
+<script>
+window.zESettings = {
+    webWidget: {
+        helpCenter: {
+            originalArticleButton: false,
+            title: {
+                '*': 'Support'
+            }
+        },
+        color: { theme: '#00765A' },
+        launcher: {
+            label: {
+                '*': 'Support'
+            }
+        }
+    }
+};
+</script>
+
+<script id="ze-snippet" src="<?php echo $scriptUrl; ?>"></script>
